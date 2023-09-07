6255b76b414ed5e5855cd1b9a193a495b4f7b591
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-09-07 04:20:16 +0300

    added maven-checkstyle-plugin and google_checks.xml

--- social-network-api/src/main/java/com/facebook/Launcher.java
+++ social-network-api/src/main/java/com/facebook/Launcher.java
@@ -1,13 +1,30 @@
-package com.facebook.facebook;
+package com.facebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FacebookApplication {
+public final class Launcher {
+    private Launcher() {
+        throw new UnsupportedOperationException("This main entry point");
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(FacebookApplication.class, args);
-	}
+    /**
+     * @param args Command Line Arguments.
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(Launcher.class, args);
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ social-network-api/src/main/java/com/facebook/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This is the root package of the application.
+ * It contains the main entry point.
+ *
+ * @author Elena Mits, Serhii Tyshchenko, Olha Khomych
+ * @version 1.0
+ * @since 2023-09-07
+ */
+package com.facebook;
