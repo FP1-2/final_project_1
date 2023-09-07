0caaa921bbf0562a225b43c261bb388c0017924f
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-09-07 16:44:20 +0300

    Launcher initialization correction

--- social-network-api/src/main/java/com/facebook/Launcher.java
+++ social-network-api/src/main/java/com/facebook/Launcher.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class Launcher {
-    private Launcher() {
-        throw new UnsupportedOperationException("This main entry point");
-    }
+public class Launcher {
 
     /**
      * @param args Command Line Arguments.
