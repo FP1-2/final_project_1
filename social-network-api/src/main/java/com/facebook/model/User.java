211dec1c38295377ebf22f2c351005d96c85dd65
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-09-09 22:22:22 +0300

    added h2 and deploy db to ukraine.com.ua

---
+++ social-network-api/src/main/java/com/facebook/model/User.java
@@ -0,0 +1,47 @@
+package com.facebook.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+@EqualsAndHashCode(callSuper = true)
+public class User extends AbstractEntity {
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String surname;
+    String address;
+    String avatar;
+    String headerPhoto;
+    private Integer dateOfBirth;
+
+    @Column(nullable = false)
+    String password;
+
+    private String roles;
+    @Transient
+    private final String DELIMITER = ":";
+
+    public void setRoles(String[] roles) {
+        this.roles = String.join(DELIMITER, roles);
+    }
+
+    public String[] getRoles() {
+        return this.roles.split(DELIMITER);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Customer{id=%d, Name=%s, Surname=%s, Address=%s, Avatar=%s, " +
+                        "Header photo=%s, Age=%s, Password=%s, Roles=%s}",
+                getId(), name, surname, address,
+                avatar, headerPhoto, dateOfBirth,
+                password, roles);
+    }
+
+}
