3c1130435bc5cd0e57bc778cae03a92dd9657ffe
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-09-11 21:32:42 +0300

    Описание ваÑadded folder structure

---
+++ social-network-api/src/main/java/com/facebook/config/web/AppConfig.java
@@ -0,0 +1,17 @@
+package com.facebook.config.web;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Використовується в пакеті `facade` для перетворення
+ * даних між шарами, (DTO) і шаром доменних моделей
+ */
+@Configuration
+public class AppConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
---
+++ social-network-api/src/main/java/com/facebook/utils/EX.java
@@ -0,0 +1,8 @@
+package com.facebook.utils;
+
+/**
+ * Використовується для нереалізованих методів
+ */
+public class EX {
+    public static final RuntimeException NI = new RuntimeException("not implemented yet");
+}
