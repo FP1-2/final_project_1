94cf8d7b2ff49cd2b3414cd5b963655a535e8088
Olha Khomych <o.fedorchuk1@gmail.com>
2023-09-17 17:53:19 +0300

    [US17] add methods for reset password

--- social-network-api/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (name, surname, username, email, address, avatar, header_photo, date_of_birth, password, roles)
-VALUES ('Olha', 'Khomych', 'okhomych', 'o.fedorchuk1@gmail.com', 'someAdress', 'avatar.png', 'header_photo.png', 1960, 'Qwert12345!', 'ADMIN');
