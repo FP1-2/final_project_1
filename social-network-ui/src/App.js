5cc926cfd4e3b6e9a1996593cf01e8e17884cbde
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-09-22 01:57:43 +0300

    corrected registration -> /registration

--- social-network-ui/src/App.js
+++ social-network-ui/src/App.js
@@ -4,7 +4,7 @@ import {Route,Routes} from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route path='registration' element={<RegistrationForm/>}/>
+      <Route path='/registration' element={<RegistrationForm/>}/>
     </Routes>
   );
 }
