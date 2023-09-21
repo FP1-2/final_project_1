2b678b1273077f31f0e47c26cfead26e6c32ebdc
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-09-22 02:18:22 +0300

    тест головного роуту

--- social-network-ui/src/App.js
+++ social-network-ui/src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <Routes>
       <Route path='/registration' element={<RegistrationForm/>}/>
+        <Route path='/' element={
+            <div style={{border: "1px solid #ccc", padding: "20px", width: "200px"}}>
+                <h2>Тест</h2>
+                <p>Тестуємо головний роут</p>
+            </div>
+        } />
     </Routes>
   );
 }
