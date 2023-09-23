3f1352276cde88a8db863b780c36e46a3b3ea9f6
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-09-23 15:36:06 +0300

    corrected Security Hotspots Math.random -> SecureRandom

--- social-network-api/src/main/java/com/facebook/utils/MathUtils.java
+++ social-network-api/src/main/java/com/facebook/utils/MathUtils.java
@@ -1,9 +1,12 @@
 package com.facebook.utils;
 
+import java.security.SecureRandom;
+
 public class MathUtils {
+    private static final SecureRandom secureRandom = new SecureRandom();
+
     public static int random(int min, int max) {
-        int range = max - min + 1; // 0 .... max - min + 1
-        double rand = Math.random() * range;
-        return (int) (rand + min);
+        int range = max - min + 1;
+        return secureRandom.nextInt(range) + min;
     }
 }
