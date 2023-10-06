64d12f9660f8d3d10c407b17bd1da8d7041c5fab
Olena Mits <dubovaya.alena@gmail.com>
2023-10-06 20:16:21 +0200

    refactor edit user

--- social-network-api/src/main/java/com/facebook/repository/FriendsRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.facebook.repository;
-//
-//import com.facebook.model.friends.Friends;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface FriendsRepository extends JpaRepository<Friends, Long> {
-//}
