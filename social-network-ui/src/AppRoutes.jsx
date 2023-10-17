7e67be8e36037ceadb97b508f2c32be81c8bc53a
Ira Bondar <irabondar1012@gmail.com>
2023-10-17 16:40:54 +0300

    add to route profile page

--- social-network-ui/src/AppRoutes.jsx
+++ social-network-ui/src/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import LoginPage from "./pages/LoginPage/LoginPage";
 import FavoritsPage from './pages/FavoritsPage/FavoritsPage';
 import MessagesPage from './pages/MessagesPage/MessagesPage';
@@ -8,18 +8,25 @@ import PostsPage from './pages/PostsPage/PostsPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
 import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
 import ConfirmRegistration from "./components/ConfirmRegistration/ConfirmRegistration";
+import PostsPageProfile from "./components/PostsPageProfile/PostsPageProfile";
+import FriendPageProfile from "./components/FriendPageProfile/FriendPageProfile";
+import LikedPageProfile from "./components/LikedPageProfile/LikedPageProfile";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage/>} />
-      <Route path="/" element={<PostsPage/>} />
-      <Route path="/messages" element={<MessagesPage/>} />
-      <Route path="/favorites" element={<FavoritsPage/>}/>
-      <Route path="/notifications" element={<NotificationsPage/>}/>
-      <Route path="/profile" element={<ProfilePage/>} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<PostsPage />} />
+      <Route path="/messages" element={<MessagesPage />} />
+      <Route path="/favorites" element={<FavoritsPage />} />
+      <Route path="/notifications" element={<NotificationsPage />} />
+      <Route path='/profile/*' element={<ProfilePage />}>
+        <Route path='' element={<PostsPageProfile />} />
+        <Route path='friends' element={<FriendPageProfile />} />
+        <Route path='liked' element={<LikedPageProfile />} />
+      </Route>
       <Route path='/registration' element={<RegistrationForm />} />
-      <Route path='/registration/confirm' element={<ConfirmRegistration/>}/>
+      <Route path='/registration/confirm' element={<ConfirmRegistration />} />
     </Routes>
   );
 }
