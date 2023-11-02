c9718784b912b304526587e26fa97b2ff04d088f
Olha Khomych <o.fedorchuk1@gmail.com>
2023-11-02 20:36:49 +0200

    [US25] add Search User on main page

--- social-network-ui/src/pages/Layout/Layout.jsx
+++ social-network-ui/src/pages/Layout/Layout.jsx
@@ -15,9 +15,12 @@ export default function Layout() {
   const excludedPaths = ['/messages', '/profile', '/login'];
   const showAside = !excludedPaths.some(path => location.pathname.startsWith(path));
 
+
   useEffect(() => {
     const vh = window.innerHeight * 0.01;
+    const vw = window.innerWidth * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.documentElement.style.setProperty('--vw', `${vw}px`);
   }, []);
 
   return (
