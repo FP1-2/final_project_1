9ce027186b1b3c2aebcfa5d4c175ebccdf71bd7d
Olha Khomych <o.fedorchuk1@gmail.com>
2023-11-06 20:18:08 +0200

    [US25] edit styles

--- social-network-ui/src/pages/Layout/Layout.jsx
+++ social-network-ui/src/pages/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+// import {useEffect} from 'react';
 import {Outlet} from "react-router";
 import Navigation from "../../components/Navigation/Navigation";
 import Header from "../../components/Header/Header";
