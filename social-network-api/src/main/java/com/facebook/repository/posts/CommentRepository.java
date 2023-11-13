ae25ea5663c6d502aab17e244ada2a660057aa51
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-11-13 10:51:01 +0200

    Added isPostLikedByCurrentUser, getCommentById and tests

--- social-network-api/src/main/java/com/facebook/repository/posts/CommentRepository.java
+++ social-network-api/src/main/java/com/facebook/repository/posts/CommentRepository.java
@@ -1,13 +1,17 @@
 package com.facebook.repository.posts;
 
+import com.facebook.dto.post.CommentDTO;
 import com.facebook.model.posts.Comment;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Репозиторій для роботи з коментарями.
@@ -18,7 +22,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     /**
      * Знаходить усі коментарі для даного поста.
      *
-     * @param postId Ідентифікатор поста.
+     * @param postId   Ідентифікатор поста.
      * @param pageable Параметри пагінації.
      * @return Сторінка з коментарями.
      */
@@ -31,6 +35,30 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
      * @return Кількість коментарів.
      */
     Long countByPostId(Long postId);
+
+    /**
+     * Здійснює пошук коментаря за ідентифікатором.
+     *
+     * @param commentId Ідентифікатор коментаря, який потрібно знайти.
+     * @return Optional об'єкт CommentDTO, якщо коментар знайдено; інакше пустий Optional.
+     */
+    @Query("""
+                SELECT new com.facebook.dto.post.CommentDTO(
+                    c.id,
+                    c.content,
+                    c.createdDate,
+                    new com.facebook.dto.post.Author(
+                        c.user.id,
+                        c.user.name,
+                        c.user.surname,
+                        c.user.username,
+                        c.user.avatar
+                    )
+                )
+                FROM Comment c
+                WHERE c.id = :commentId
+            """)
+    Optional<CommentDTO> findCommentDtoById(@Param("commentId") Long commentId);
 
     void deleteByPostId(Long postId);
 
