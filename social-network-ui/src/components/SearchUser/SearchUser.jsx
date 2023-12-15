b279fa7e530b48ef580f9c05958a1e868376ae68
Olha Khomych <o.fedorchuk1@gmail.com>
2023-12-16 00:21:18 +0200

    update search field in chat nav

--- social-network-ui/src/components/SearchUser/SearchUser.jsx
+++ social-network-ui/src/components/SearchUser/SearchUser.jsx
@@ -26,7 +26,7 @@ export default function SearchUser({ handleBack, textSearch, setTextSearch}) {
     setTextSearch('');
   }
   function addRecentSearch (searchUser){
-    setRecentSearch([searchUser, ...recentSearch.slice(0,9)]);
+    setRecentSearch([searchUser, ...recentSearch]);
   }
   return (
     <div className={styles.searchUser} id="search-user-portal" >
