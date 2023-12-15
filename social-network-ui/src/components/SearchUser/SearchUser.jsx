1cb020f0027e6920c93d875ba272fc06a093ea50
Olha Khomych <o.fedorchuk1@gmail.com>
2023-12-15 23:25:23 +0200

    update search field

--- social-network-ui/src/components/SearchUser/SearchUser.jsx
+++ social-network-ui/src/components/SearchUser/SearchUser.jsx
@@ -56,12 +56,12 @@ export default function SearchUser({ handleBack, textSearch, setTextSearch}) {
       <ul className={styles.searchUser__filteredUsers}>
         {searchUsers.status === '' ? 
           (recentSearch.length === 0 ? 
-            <p>enter name/username for searching user</p>
+            <p className={styles.searchUser__filteredUsers__text}>enter name/username for searching user</p>
             : 
             (<React.Fragment>
-              <p>Recently:</p>
+              <p className={styles.searchUser__filteredUsers__recetly}>Recently:</p>
               {recentSearch.map(({ id, avatar, name, surname }) => (
-                <li key={id} onClick={()=> closePortal()} 
+                <li key={id} onClick={()=> closePortal} 
                   className={styles.searchUser__filteredUsers__item}>
                   <NavLink to={`/profile/${id}`} className={styles.searchUser__filteredUsers__item__link} >
                     <ChatItem photo={avatar} name={name + ' ' + surname} additionalClass={styles.searchUser__filteredUsers__item__link__user} />
@@ -72,12 +72,12 @@ export default function SearchUser({ handleBack, textSearch, setTextSearch}) {
           )
           : (searchUsers.status === 'pending' ? <Loader/> :
             (searchUsers.obj.length === 0 ? 
-              <p>no results</p>
+              <p  className={styles.searchUser__filteredUsers__text}>no results</p>
               :
               searchUsers.obj.map(({ id, avatar, name, surname }) => (
                 <li key={id} onClick={() => {
                   addRecentSearch({ id, avatar, name, surname });
-                  closePortal();}} 
+                  closePortal;}} 
                 className={styles.searchUser__filteredUsers__item}>
                   <NavLink to={`/profile/${id}`} className={styles.searchUser__filteredUsers__item__link} >
                     <ChatItem photo={avatar} name={name + ' ' + surname} additionalClass={styles.searchUser__filteredUsers__item__link__user} />
