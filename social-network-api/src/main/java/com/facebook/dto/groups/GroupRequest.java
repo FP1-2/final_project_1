24b732f0c5af71270e18b04c95d539c2562f6c9b
Serhii Tishchenko <yourhostel.ua@gmail.com>
2023-12-16 12:49:23 +0200

    added genGrops

--- social-network-api/src/main/java/com/facebook/dto/groups/GroupRequest.java
+++ social-network-api/src/main/java/com/facebook/dto/groups/GroupRequest.java
@@ -1,9 +1,11 @@
 package com.facebook.dto.groups;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class GroupRequest {
     @NotBlank(message = "Name is required")
     private String name;
